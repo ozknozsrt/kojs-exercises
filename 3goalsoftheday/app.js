b9668dc5f50861b0e5762ab0aac59a6f4db56931
Özkan ÖZSERT <ozkanozsert@gmail.com>
2020-10-28 18:04:33 +0300

    Add files via upload

---
+++ 3goalsoftheday/app.js
@@ -0,0 +1,93 @@
+var vm = function () {
+  var self = this;
+  self.input = ko.observable("");
+  self.data = ko.observableArray([]);
+  self.countdown = ko.observable("");
+
+  // init Lang
+  self.language = ko.observable("en");
+  self.language.subscribe(function (value) {
+    i18nextko.setLanguage(value);
+  });
+  self.ph = i18nextko.t("placeholder");
+  // */init Lang
+
+  self.addGoal = function () {
+    if (self.input() != "") {
+      self.data.push(document.getElementById("goalInput").value);
+      self.input("");
+    }
+
+    if (self.data().length >= 3) {
+      $("#submitBtn").attr("disabled", true);
+      $("#goalInput").attr("disabled", true);
+    }
+  };
+
+  var timer = null,
+    interval = 1000;
+
+  function timeText(timer, s, dk, sn) {
+    if (timer == null) {
+      if (self.language() == "en") {
+        return "You Stopped Your Goal!";
+      } else {
+        return "Hedefinizi Durdurdunuz!";
+      }
+    } else {
+      if (self.language() == "en") {
+        return `You have last <b>${s} hours ${dk} mins ${sn} secs</b> to your goals`;
+      } else {
+        return `Hedefinize kalan son <b>${s} saat ${dk} dk ${sn} sn</b>`;
+      }
+    }
+  }
+
+  self.startCounter = function () {
+    if (timer !== null) return;
+
+    // Current Time
+    var crTime = new Date();
+    crTime.setDate(crTime.getDate() + 1);
+
+    var countDownDate = new Date(crTime).getTime();
+
+    // Update the count down every 1 second
+    timer = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      self.countdown(timeText("", hours, minutes, seconds));
+
+      // self.countdown(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(timer);
+        self.countdown("Süreniz Doldu!");
+      }
+    }, interval);
+  };
+
+  self.stopCounter = function () {
+    clearInterval(timer);
+    timer = null;
+    self.countdown(timeText(timer));
+    $("#stopCounter").hide();
+    $("#startCounter").show();
+  };
+};
+
+ko.applyBindings(new vm(), document.getElementById("app"));
---
+++ 3goalsoftheday/lang.js
@@ -0,0 +1,49 @@
+i18nextko.init(
+  {
+    en: {
+      translation: {
+        choiceLang: 'Language',
+        placeholder: 'Enter your goal',
+        submitBtn: 'Add Goal',
+        cardHeader: 'Your goals today',
+        nogoal: 'You do not have a goal yet.',
+        startCounter: 'Start Counter',
+        stopCounter: 'Stop Counter',
+        stoppedCounter: 'You Stopped Your Goal!',
+        startedCounter: 'The last <b>{{s}} hours {{dk}} mins {{sn}} secs</b> left to your goals',
+
+        title: "i18next-ko Example",
+        text: "This is an example for i18next-ko. Please select a language:",
+        greet: "Hello, {{name}}!",
+        dummyButton: "Pointless button",
+        dummyButtonFor: "Your very own pointless button, {{name}}!",
+        dummySpan: "Your very own pointless <span>",
+        translatedVariable:
+          "This is a computed observable, that is updated on language changes.",
+      },
+    },
+    tr: {
+      translation: {
+        choiceLang: 'Dil Seçiniz',
+        placeholder: 'Hedefinizi girin',
+        submitBtn: 'Hedef Ekle',
+        cardHeader: 'Bugünkü Hedefleriniz',
+        nogoal: 'Henüz bir hedefiniz bulunmamaktadır.',
+        startCounter: 'Sayacı Başlat',
+        stopCounter: 'Sayacı Durdur',
+        stoppedCounter: 'Hedefinizi Durdurdunuz!',
+        startedCounter: 'Hedefinize kalan son <b>{{s}} saat {{dk}} dk {{sn}} sn</b>',
+
+        title: "<h3>i18next-ko Beispiel</h3>",
+        text: "Dies ist ein Beispiel für i18next-ko. Wählen Sie eine Sprache:",
+        greet: "Hallo, {{name}}!",
+        dummyButton: "Sinnloser Knopf",
+        dummyButtonFor: "Dein ganz persönlicher sinnloser Knopf, {{name}}!",
+        translatedVariable:
+          "Dies ist ein computed observable, der sich beim Sprachwechsel aktualisiert.",
+      },
+    },
+  },
+  "en",
+  ko
+);
